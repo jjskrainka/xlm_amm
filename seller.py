387ba709075c439f2fefe79fc94ce69896c2996f
jjskrainka <jjskrainka@gmail.com>
2022-03-23 20:29:08 -0500

    feat: fixed buyer and seller to use liquidity pool op instead of strict ops

--- seller.py
+++ seller.py
@@ -4,7 +4,7 @@ This class simulates a bidder for the amm liquidity pool. It sources a price fro
 from stellar_sdk import Account, Asset, Keypair, Network, TransactionBuilder, LiquidityPoolAsset
 from helpers import get_sequence, connect_horizon, create_trust
 from price import Price
-from stellar_sdk.operation.path_payment_strict_send import PathPaymentStrictSend
+from stellar_sdk.operation.path_payment_strict_receive import PathPaymentStrictReceive
 from liquidity_pool import get_lp_id
 import os
 
@@ -13,44 +13,42 @@ import os
 # that given spread. The larger the spread, the more favorable the transaction, so the overall AMOUNT would be greater.
 LEVELS = [[0.025, 1], [0.005, 2], [0.02, 3]]
 
-buyer_secret =  os.environ.get("SELLER_SECRET") 
-buyer_keypair = Keypair.from_secret(
-        buyer_secret
+seller_secret = os.environ.get("SELLER_SECRET")
+seller_keypair = Keypair.from_secret(
+            seller_secret
 )
-sequence = get_sequence(buyer_keypair.public_key)
+sequence = get_sequence(seller_keypair.public_key)
 
 # Create an Account object from an address and sequence number.
-buyer_account = Account(account=buyer_keypair.public_key, sequence=sequence)
+seller_account = Account(account=seller_keypair.public_key, sequence=sequence)
 reference_price = Price().main()
 lp_account_id = os.environ.get("LIQUIDITY_POOL")
 
 destination =  get_lp_id()
 send_asset = Asset.native()
-send_amount = LEVELS[0][1]
+send_max = LEVELS[0][1]
 dest_asset = LiquidityPoolAsset(Asset.native(), Asset("USD", lp_account_id))  # liquidity pool asset 
-dest_min = round(reference_price - LEVELS[0][0], 7)
-path = [send_asset, dest_asset]
+dest_amount = reference_price + LEVELS[0][0]
 
-create_trust(buyer_account, dest_asset, buyer_secret)
+create_trust(seller_account, dest_asset, seller_secret)
 transaction = (
     TransactionBuilder(
-        source_account=buyer_account,
+        source_account=seller_account,
         network_passphrase=Network.TESTNET_NETWORK_PASSPHRASE,
         base_fee=100,
     )
-    .add_text_memo("Making the market as a buyer")
-    .append_liquidity_pool_deposit_op(
+    .add_text_memo("Making market as a seller")
+    .append_liquidity_pool_withdraw_op(
         liquidity_pool_id=destination,
-        max_amount_a=str(11 / 1000000),
-        max_amount_b=str(1 / 1000000),
-        min_price=str(1 / (reference_price + 1) / 1000000),
-        max_price=str(1 / reference_price)
+        amount=str(round(LEVELS[0][1] / 10000000, 7)),
+        min_amount_a=str(round(1 / 1000000, 7)),  # because we are referencing USD price of 1 XLM
+        min_amount_b=str(round(dest_amount / 1000000, 7))  # reference price in USD with spread
     )
     .set_timeout(30)
     .build()
 )
 print(transaction)
-transaction.sign(buyer_keypair)
+transaction.sign(seller_keypair)
 response = connect_horizon().submit_transaction(transaction)
 print(response["successful"])
 
