af1838f177d66567ba9cbab86738b4f2a5a8a9d4
jjskrainka <jjskrainka@gmail.com>
2022-03-26 19:57:17 -0500

    feat: improve order book to use as a reference of other liquidity pool which internal liquidity pool can try to beat

--- order_book.py
+++ order_book.py
@@ -1,3 +1,6 @@
+"""
+Class to query most popular USDC/XLM liquidity pool order book to try to provide a better exchange rate and boost my own LP usage
+"""
 import requests as r
 from stellar_sdk import Server, Asset
 from stellar_sdk.call_builder.base.base_orderbook_call_builder import BaseOrderbookCallBuilder
@@ -7,11 +10,21 @@ from helpers import connect_horizon
 
 class OrderBook(object):
     server = connect_horizon()
-    usd = Asset("USD", os.environ.get("LIQUIDITY_POOL"))
+    server = Server(horizon_url="https://horizon.stellar.org")
+    usd = Asset("USDC", "GA5ZSEJYB37JRC5AVCIA5MOP4RHTM335X2KGX3IHOJAPP5RE34K4KZVN")  # use most popular USDC pool to try and beat
     native = Asset.native()
     ob = server.orderbook(native, usd)
-    print(ob.call())
+    ob = ob.call()
+    asks = ob["asks"]
+    bids = ob["bids"]
+    print("-"*25, "\nBIDS: ", bids)
+    print("-"*25, "\nASKS: ", asks)
 
+    def get_bids(self):
+        return bids
+
+    def get_asks(self):
+        return asks
 
 if __name__ == "__main__":
     OrderBook()
